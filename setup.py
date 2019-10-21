9cb1491e1d195995d85612cef15a4377d2baff7f
Praseetha-KR <praseetha04@gmail.com>
2019-10-21 19:13:17 +0530

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='pyjarsigner',
-    version='0.2.0',
+    version='0.2.1',
     url='https://github.com/appknox/pyjarsigner',
 
     author='Subho Halder',
