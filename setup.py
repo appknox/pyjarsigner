d4438c9671d5847622edc37b8f6fde1945a52b0e
surendrajat <surendrajat@protonmail.com>
2023-08-28 15:39:43 +0530

    bump version to v0.2.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='pyjarsigner',
-    version='0.2.2',
+    version='0.2.3',
     url='https://github.com/appknox/pyjarsigner',
 
     author='Subho Halder',
